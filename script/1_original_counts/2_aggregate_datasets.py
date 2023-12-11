15bd9c64df08f709502bac5731aecab4053a2e57
Marta Sallese <martarosa.sallese@ieo.it>
2023-12-11 14:46:16 +0100

    typo edits

--- script/1_original_counts/2_aggregate_datasets.py
+++ script/1_original_counts/2_aggregate_datasets.py
@@ -32,7 +32,7 @@ data = ['/group/testa/Project/OvarianAtlas/atlas_project/raw_data/original_count
         '/group/testa/Project/OvarianAtlas/atlas_project/raw_data/original_counts/Zhang2022/Adata/zhang2022_rawcounts.h5ad',
         '/group/testa/Project/OvarianAtlas/atlas_project/raw_data/original_counts/Regner2021/Adata/regner2021_rawcounts.h5ad',
         '/group/testa/Project/OvarianAtlas/atlas_project/raw_data/original_counts/Ren2022/Adata/ren2022_rawcounts.h5ad',
-        '/group/testa/Project/OvarianAtlas/atlas_project/raw_data/original_counts/Qian2020/Adata/qian2020_rawcounts.h5ad'
+        '/group/testa/Project/OvarianAtlas/atlas_project/raw_data/original_counts/Qian2020/Adata/qian2020_rawcounts.h5ad',
         '/group/testa/Project/OvarianAtlas/atlas_project/raw_data/original_counts/Vasquez2022/Adata/vasquez2022_rawcounts.h5ad']
 
 for i in data:
@@ -41,7 +41,3 @@ for i in data:
 adata.obs = adata.obs.drop(columns=['batch'])
 
 adata.write_h5ad('/group/testa/Project/OvarianAtlas/atlas_project/raw_data/original_counts/atlas_rawcounts.h5ad')
-
-# nb_fname = "atlas_rawcounts"
-
-# get_ipython().run_cell_magic('bash', '-s "$nb_fname"', 'jupyter nbconvert "$1".ipynb --to="python"\njupyter nbconvert "$1".ipynb --to="html"')
