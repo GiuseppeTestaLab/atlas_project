7128ad9bfb3120dc1de256dc19f9fdc91576fa32
Marta Sallese <martarosa.sallese@ieo.it>
2024-02-09 17:49:43 +0100

    created files to generate sankey plots

---
+++ script/7_downstream/clustering/cancer/2_sankey/primary_sankey.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=8
+#SBATCH --partition=cpuq
+#SBATCH --job-name=primary
+#SBATCH --mem=100GB
+#SBATCH --mail-type=ALL
+#SBATCH --output=%x_%j.log 
+module load singularity
+
+singularity run -B /group/testa -B /run/user -B $TMPDIR:/tmp \
+-B /home/marta.sallese -H /home/marta.sallese/ov_cancer_atlas \
+docker://testalab/downstream:covidiamo-3.1.0 \
+"/home/marta.sallese/miniconda3/envs/scvi_plots/bin/python /home/marta.sallese/ov_cancer_atlas/atlas_project/script/7_downstream/clustering/cancer/2_sankey/primary_sankey.py"
