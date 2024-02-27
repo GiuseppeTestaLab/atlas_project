a4e49600dbcded276bb97d0606fafbb3c56e0049
Marta Sallese <martarosa.sallese@ieo.it>
2024-02-27 12:45:30 +0100

    fixed error

--- script/6_integration/metacells/cancer/scVI_labels_tissuetreatment_HDG.sh
+++ script/6_integration/metacells/cancer/scVI_labels_tissuetreatment_HDG.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks=8
+#SBATCH --ntasks-per-node=8
 #SBATCH --partition=cpuq
 #SBATCH --job-name=seacells_scVI
 #SBATCH --mem=200GB
