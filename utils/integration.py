8fe829e8ca9ec1073c70766f816a67017877cdf6
Marta Sallese <martarosa.sallese@ieo.it>
2024-02-27 12:47:42 +0100

    fixed error

--- utils/integration.py
+++ utils/integration.py
@@ -13,6 +13,17 @@ def preprocess_scgen(ad, batch, genes):
     sc.tl.umap(ad)
     return(ad)
 
+def preprocess_scgen_genes(ad, batch, genes):
+    #sc.pp.normalize_total(ad)
+    sc.pp.log1p(ad)
+    sc.pp.highly_variable_genes(ad, batch_key=batch, n_top_genes=genes, inplace=True)
+    ad.raw = ad
+    ad = ad[:, ad.var.highly_variable]
+    sc.tl.pca(ad, use_highly_variable=True)
+    sc.pp.neighbors(ad, use_rep='X_pca')
+    sc.tl.umap(ad)
+    return(ad)
+
 def preprocess_scVI(adata, batch, genes):
     sc.pp.normalize_total(adata, target_sum=1e4)
     sc.pp.log1p(adata)
