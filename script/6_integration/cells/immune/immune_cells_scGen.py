85290befc251759b5b62e0a6291fc1360f6f1550
Marta Sallese <martarosa.sallese@ieo.it>
2024-02-27 12:41:51 +0100

    immune cells integration

--- script/6_integration/cells/immune/immune_cells_scGen.py
+++ script/6_integration/cells/immune/immune_cells_scGen.py
@@ -5,7 +5,7 @@ import scanpy as sc
 import scgen
 import sys
 sys.path.insert(1, '/home/marta.sallese/ov_cancer_atlas/atlas_project/utils')
-from integration import preprocess_scgen
+from integration import preprocess_scgen_genes
 
 #%%
 initDir = '/group/testa/Project/OvarianAtlas/atlas_project/raw_data/metacells/immune/'
@@ -50,7 +50,7 @@ ad.obs['cell_types'] = ad.obs[['Plasma_cells', 'T_cells','Mast_cells', 'B_cells'
 #%%
 batch = 'paper_ID'
 genes = 2500
-ad = preprocess_scgen(ad, batch, genes)
+ad = preprocess_scgen_genes(ad, batch, genes)
 
 #%%
 scgen.SCGEN.setup_anndata(ad, batch_key="paper_ID", labels_key="cell_types")
@@ -70,4 +70,39 @@ model.train(
 #%%
 corrected_adata = model.batch_removal()
 corrected_adata.write_h5ad(outDir + 'scgen_batch_corr_celltypes.h5ad')
+
+## Visualization of the corrected adata
+
+#%%
+sc.settings.set_figure_params(dpi_save=300, frameon=False, format='png')
+sc.settings.figdir = "/home/marta.sallese/ov_cancer_atlas/atlas_project/plots_def/integration/cells/immune/"
+
+#%%
+adata = sc.read(outDir + 'scgen_batch_corr_celltypes.h5ad')
+
+#%%
+cell_cycle_genes = [x.strip() for x in open('/home/marta.sallese/ov_cancer_atlas/regev_lab_cell_cycle_genes.txt')]
+
+s_genes = cell_cycle_genes[:43]
+g2m_genes = cell_cycle_genes[43:]
+cell_cycle_genes = [x for x in cell_cycle_genes if x in adata.var_names]
+
+sc.tl.score_genes_cell_cycle(adata, s_genes=s_genes, g2m_genes=g2m_genes, use_raw=True)
+
+#%%
+sc.tl.pca(adata)
+sc.pp.neighbors(adata, n_neighbors=10, n_pcs=50)
+sc.tl.umap(adata)
+
+#%%
+sc.pl.umap(adata, color=["treatment"], frameon=False, save='immune_cells_treatm.png')
+sc.pl.umap(adata, color=["tissue"], frameon=False, save='immune_cells_tissue.png')
+sc.pl.umap(adata, color=["dataset"], frameon=False, save='immune_cells_dataset.png')
+sc.pl.umap(adata, color=["paper_ID"], frameon=False, save='immune_cells_patients.png')
+sc.pl.umap(adata, color=["phase"], frameon=False, save='immune_cells_cellcycle.png')
+sc.pl.umap(adata, color=["tissue-treatment"], frameon=False, save='immune_cells_tissue-treat.png')
+sc.pl.umap(adata, color=["cell_types"], frameon=False, save='immune_cells_HDG_celltypes.png')
+
+#%%
+adata.write(outDir + 'scgen_batch_corr_celltypes_embeddings.h5ad')
 
--- script/6_integration/cells/immune/immune_cells_scGen_HDG.py
+++ script/6_integration/cells/immune/immune_cells_scGen_HDG.py
@@ -6,7 +6,7 @@ import scgen
 import pandas as pd
 import sys
 sys.path.insert(1, '/home/marta.sallese/ov_cancer_atlas/atlas_project/utils')
-from integration import preprocess_scgen
+from integration import preprocess_scgen_genes
 
 #%%
 initDir = '/group/testa/Project/OvarianAtlas/atlas_project/raw_data/metacells/immune/'
@@ -54,6 +54,8 @@ sc.pp.log1p(ad)
 hdg = pd.read_csv(genes, index_col=0)
 ad.var['highly_variable'] = hdg.highly_variable
 ad.var.highly_variable = ad.var.highly_variable.fillna(False)
+ad.raw = ad
+ad = ad[:, ad.var.highly_variable]
 sc.tl.pca(ad, use_highly_variable=True)
 sc.pp.neighbors(ad, use_rep='X_pca')
 sc.tl.umap(ad)
@@ -76,4 +78,39 @@ model.train(
 #%%
 corrected_adata = model.batch_removal()
 corrected_adata.write_h5ad(outDir + 'scgen_batch_corr_celltypes_HDG.h5ad')
+
+## Visualization of the corrected adata
+
+#%%
+sc.settings.set_figure_params(dpi_save=300, frameon=False, format='png')
+sc.settings.figdir = "/home/marta.sallese/ov_cancer_atlas/atlas_project/plots_def/integration/cells/immune/"
+
+#%%
+adata = sc.read(outDir + 'scgen_batch_corr_celltypes_HDG.h5ad')
+
+#%%
+cell_cycle_genes = [x.strip() for x in open('/home/marta.sallese/ov_cancer_atlas/regev_lab_cell_cycle_genes.txt')]
+
+s_genes = cell_cycle_genes[:43]
+g2m_genes = cell_cycle_genes[43:]
+cell_cycle_genes = [x for x in cell_cycle_genes if x in adata.var_names]
+
+sc.tl.score_genes_cell_cycle(adata, s_genes=s_genes, g2m_genes=g2m_genes, use_raw=True)
+
+#%%
+sc.tl.pca(adata)
+sc.pp.neighbors(adata, n_neighbors=10, n_pcs=50)
+sc.tl.umap(adata)
+
+#%%
+sc.pl.umap(adata, color=["treatment"], frameon=False, save='immune_cells_HDG_treatm_HDG.png')
+sc.pl.umap(adata, color=["tissue"], frameon=False, save='immune_cells_HDG_tissue_HDG.png')
+sc.pl.umap(adata, color=["dataset"], frameon=False, save='immune_cells_HDG_dataset_HDG.png')
+sc.pl.umap(adata, color=["paper_ID"], frameon=False, save='immune_cells_HDG_patients_HDG.png')
+sc.pl.umap(adata, color=["phase"], frameon=False, save='immune_cells_HDG_cellcycle_HDG.png')
+sc.pl.umap(adata, color=["cell_types"], frameon=False, save='immune_cells_HDG_celltypes_HDG.png')
+sc.pl.umap(adata, color=["tissue-treatment"], frameon=False, save='immune_cells_HDG_tissue-treat_HDG.png')
+
+#%%
+adata.write(outDir + 'scgen_batch_corr_celltypes_embeddings_HDG.h5ad')
 
