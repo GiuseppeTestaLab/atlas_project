576236fb926386c761b7df91ae5cbb9517716bde
Marta Sallese <martarosa.sallese@ieo.it>
2024-03-05 14:00:07 +0100

    testing new metacells integration spaces

--- script/6_integration/metacells/fibroblasts/scGen_labels_celltypes_2500.py
+++ script/6_integration/metacells/fibroblasts/scGen_labels_celltypes_2500.py
@@ -18,6 +18,28 @@ ad = sc.read(initDir + "seacells_hdg_patients.h5ad")
 ad.obs['tissue-treatment'] = ad.obs['tissue'].astype('str') + '_' + ad.obs['treatment'].astype('str')
 
 #%%
+# inflammatory CAFs (iCAF)
+sc.tl.score_genes(ad, ['IL6', 'CXCL12', 'VTN', 'CADM3', 'PLIN2', 'SERPINB2', 'KRT19', 'DES', 'CALB2', 'WT1', 'KRT7'], 
+score_name = "iCAF", use_raw=False)
+
+# matrix CAFs (mCAF)
+sc.tl.score_genes(ad, ['ACTA2', 'COL11A1', 'MFAP5', 'SFRP2', 'ISLR', 'COL10A1', 'VIM', 'COL3A', 
+                       'MMP11', 'PTHLH', 'FGF1', 'WNT7B', 'WNT2', 'TGFB3', 'THRC1', 'POSTN', 'VCAN', 'ZEB1'], 
+score_name = "mCAF", use_raw=False)
+
+# vascular cancer-associated fibroblasts CAFs (vCAF)
+sc.tl.score_genes(ad, ['COX4I2', 'HIGD1B', 'PTP4A3', 'MCAM', 'PPP1R14A'], 
+score_name = "vCAF", use_raw=False)
+
+# STAR gene expressing CAFs (starCAF)
+sc.tl.score_genes(ad, ['STAR', 'IGFBP5', 'TSPAN8', 'C7', 'ALDH1A1', 'LGR5'], 
+score_name = "starCAF", use_raw=False)
+
+#%%
+ad.obs['cell_types'] = ad.obs[['iCAF', 'mCAF','vCAF', 'starCAF']].idxmax(axis=1)
+ad.obs
+
+#%%
 batch = 'dataset'
 genes = 2500
 ad = preprocess_scgen_genes(ad, batch, genes)
--- script/6_integration/metacells/immune/scGen_labels_celltypes_HDG.py
+++ script/6_integration/metacells/immune/scGen_labels_celltypes_HDG.py
@@ -16,6 +16,39 @@ ad = sc.read(initDir + "seacells_hdg_patients.h5ad")
 ad.obs['tissue-treatment'] = ad.obs['tissue'].astype('str') + '_' + ad.obs['treatment'].astype('str')
 
 #%%
+# Plasma cells markers
+sc.tl.score_genes(ad, ['IGKC','IGHG1','CD79A','IGHG2','IGLC2','IGLC3','IGHG3','IGHG4','JCHAIN','MZB1','XBP1'], 
+score_name = "Plasma_cells", use_raw=False)
+
+# T cells markers
+sc.tl.score_genes(ad, ['CD2','CD3D','TRAC','GZMA','NKG7','CD3E','CD3G','CD4','TCF7','CD8A','PRF1','GZMB','CCL5','CCL4','IL32','CD52'], 
+score_name = "T_cells", use_raw=False)
+
+# Mast cells markers
+sc.tl.score_genes(ad, ['KIT','CPA3','CTSG','MS4A2','TPSAB1','TPSB2','HPGD','HPGDS','GATA2'], 
+score_name = "Mast_cells", use_raw=False)
+
+# B cells markers
+sc.tl.score_genes(ad, ['MS4A1', 'CD79A', 'CD19', 'BANK1', 'IGKC', 'IGHM'], score_name = "B_cells", use_raw=False)
+
+# Myeloid cells markers
+sc.tl.score_genes(ad, 
+['CD14','FCER1G','FCGR3A','LYZ','CTSS','CD33','CD68','CD163','ITGAX','ITGAM','CD4','MRC1',
+'VSIG4','SPP1','APOE','C1QA','C1QB','C1QC','APOC1','FTL','S100A9','TYROBP','AIF1','CD74','PSAP','CTSB'], 
+score_name = "Myeloid_cells", use_raw=False)
+
+# Dendritic cells markers
+sc.tl.score_genes(ad, 
+['IL3RA','IRF7','IRF8','GZMB','CD4','CLEC4C','JCHAIN',
+'PTGDS','PLAC8','PLD4','TCF4','BCL11A','GPR183','CCDC50','LILRA4','TSPAN13','CLIC3','MPEG1'], 
+score_name = "Dendritic_cells", use_raw=False)
+
+#%%
+ad.obs
+ad
+ad.obs['cell_types'] = ad.obs[['Plasma_cells', 'T_cells','Mast_cells', 'B_cells', 'Myeloid_cells', 'Dendritic_cells' ]].idxmax(axis=1)
+
+#%%
 sc.pp.log1p(ad)
 hdg = pd.read_csv(genes, index_col=0)
 ad.var['highly_variable'] = hdg.highly_variable
