22f7a8a89707ff3a82b2ad6157a5db5ee3f3ae88
Marta Sallese <martarosa.sallese@ieo.it>
2024-03-15 13:59:36 +0100

    error fix

--- script/7_downstream/clustering/endothelial/3_cluster_assignments/3_plots_metacells.py
+++ script/7_downstream/clustering/endothelial/3_cluster_assignments/3_plots_metacells.py
@@ -22,22 +22,22 @@ primary.obs['cell_states'] = np.nan
 
 col = {"0":"Immunoreactive_cells-T_cells", 
         "1":"Immunoreactive_cells-neutrophils", 
-        "2":"Unknown_primary_2", 
+        "2":"Unknown_primary", 
         "3":"Cellular_metabolism", 
         "4":"Angiogenesis", 
-        "5":"Unknown_primary_5", 
-        "6":"Unknown_primary_6",
+        "5":"Unknown_primary", 
+        "6":"Unknown_primary",
         "7":"RNA_metabolism", 
-        "8":"Unknown_primary_8", 
+        "8":"Unknown_primary", 
         "9":"Immunoreactive_cells", 
         "10":"Cellular_metabolism", 
         "11":"Cycling_cells", 
-        "12":"Unknown_primary_12"}
+        "12":"Unknown_primary"}
 
 primary.obs['cell_states'] = primary.obs['leiden-0.51'].replace(col)
 
 sc.pl.umap(primary, color=['cell_states'], frameon=False, save='_primary_cell_states.png')
-sc.pl.umap(primary, color=['leiden-0.51'], frameon=False, save='_priamry_leiden051.png')
+sc.pl.umap(primary, color=['leiden-0.51'], frameon=False, save='_primary_leiden051.png')
 sc.pl.umap(primary, color=['treatment'], frameon=False, save='_primary_treatment.png')
 sc.pl.umap(primary, color=['phase'], frameon=False, save='_primary_cellcycle.png')
 
@@ -49,19 +49,19 @@ metastasis.obs['cell_states'] = np.nan
 col = { "0":"Immunoreactive_cells-T_cells", 
         "1":"Immunoreactive_cells-neutrophils", 
         "2":"Angiogenesis", 
-        "3":"Unknown_metastasis_3", 
+        "3":"Unknown_metastasis", 
         "4":"Immunoreactive_cells-B_cells", 
         "5":"Cellular_metabolism",
         "6":"Immunoreactive_cells-neutrophils", 
-        "7":"Unknown_metastasis_7", 
+        "7":"Unknown_metastasis", 
         "8":"Immunoreactive_cells-T_cells",
         "9":"Immunoreactive_cells",
         "10":"Immunoreactive_cells-T_cells",
         "11":"Cycling_cells",
         "12":"RNA_metabolism",
         "13":"Immunoreactive_cells-neutrophils",
-        "14":"Unknown_metastasis_14",
-        "15":"Unknown_metastasis_15"} 
+        "14":"Unknown_metastasis",
+        "15":"Unknown_metastasis"} 
 
 metastasis.obs['cell_states'] = metastasis.obs['leiden-0.51'].replace(col)
 
@@ -75,7 +75,7 @@ metastasis.write_h5ad(tissueDir + 'adata_metastasis_embeddings_cellstates.h5ad')
 #%%
 ascites.obs['cell_states'] = np.nan
 
-col = { "0":"Unknown_ascites_0", 
+col = { "0":"Unknown_ascites", 
         "1":"Immunoreactive_cells-T_cells", 
         "2":"Immunoreactive_cells-neutrophils", 
         "3":"Immunoreactive_cells-neutrophils", 
