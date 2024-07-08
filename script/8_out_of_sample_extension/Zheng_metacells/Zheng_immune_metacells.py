2897c39ad0767aaa5bfeac5d10250f7be89f0214
Marta Sallese <martarosa.sallese@ieo.it>
2024-07-08 16:49:58 +0200

    zheng metacells generation

---
+++ script/8_out_of_sample_extension/Zheng_metacells/Zheng_immune_metacells.py
@@ -0,0 +1,83 @@
+# Zheng immune metacells generation
+
+## Import libraries
+#%%
+import numpy as np
+import pandas as pd
+import scanpy as sc
+import SEACells
+
+## Load data
+#%%
+adata= sc.read("/group/testa/Project/OvarianAtlas/Zheng2023/Adata/zheng_immune_filt_norm_nolog.h5ad")
+
+
+## Preprocessing
+#%%
+sc.pp.neighbors(adata)
+raw_ad = sc.AnnData(adata.X)
+raw_ad.obs_names, raw_ad.var_names = adata.obs_names, adata.var_names
+adata.raw = raw_ad
+sc.pp.log1p(adata)
+hdg = pd.read_csv('/home/marta.sallese/ov_cancer_atlas/Atlas_scripts/HDG_new/Tables/atlas_hdg_dispersion_patients_immune.csv',  index_col=0)
+hdg[hdg.highly_variable]
+adata.var['highly_variable']=hdg.highly_variable
+adata.var.highly_variable = adata.var.highly_variable.fillna(False)
+sc.tl.pca(adata, n_comps=50, use_highly_variable=True)
+sc.tl.umap(adata)
+#adata.write_h5ad('/group/testa/Project/OvarianAtlas/Zheng2023/Adata/zheng_immune_embeddings.h5ad')
+
+## Metacells generation per patient
+#%%
+sc.settings.verbosity = 0
+models = {}
+mc_obs = pd.DataFrame()
+i=0
+for patient in adata.obs.paper_ID.unique():
+    ad_patient=adata[adata.obs.paper_ID==patient].copy()
+    n_SEACells = np.round(ad_patient.shape[0]/75, 0)
+    build_kernel_on = 'X_pca'
+    n_waypoint_eigs = 10 
+    model = SEACells.core.SEACells(ad_patient, 
+                                   build_kernel_on=build_kernel_on, 
+                                   n_SEACells=n_SEACells, 
+                                   n_waypoint_eigs=n_waypoint_eigs, 
+                                   convergence_epsilon = 1e-5,verbose=False)
+    model.construct_kernel_matrix()
+    M = model.kernel_matrix 
+    try:
+        model.initialize_archetypes()
+    except IndexError as e:
+        print('patient {} the number {} has too few cells'.format(patient,i))
+        ad = adata[adata.obs.paper_ID != patient]
+        continue
+    model.fit(min_iter=10, max_iter=200)
+    models[patient]=model
+    labels,weights = model.get_soft_assignments()
+    SEACell_soft_ad = SEACells.core.summarize_by_soft_SEACell(ad_patient, model.A_, celltype_label='paper_ID',summarize_layer='raw', minimum_weight=0.05)
+    if mc_obs.shape[0] == 0:
+        # mc_obs=SEACell_soft_ad
+        mc_obs=ad_patient.obs
+    else:
+        mc_obs=pd.concat([mc_obs,ad_patient.obs])
+        # mc_obs=mc_obs.concatenate(SEACell_soft_ad)
+    i+=1
+
+    print('done! patient {} the number {}'.format(patient,i))
+    #if i>2:
+    #    break
+sc.settings.verbosity = 3  
+
+import pickle
+
+with open('models.pkl','wb') as file:
+    pickle.dump(models,file)
+
+
+with open('models.pkl','rb') as file:
+
+    modelsLoaded=pickle.load(file)
+
+adata.obs['SEACell'] = mc_obs.SEACell
+
+adata.write_h5ad('/group/testa/Project/OvarianAtlas/Zheng2023/Metacells/immune_seacells_assignment_hdg_patients.h5ad')
---
+++ script/8_out_of_sample_extension/Zheng_metacells/zheng_fibroblast_metacells.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --partition=cpuq
+#SBATCH --job-name=fibroblast_metacells
+#SBATCH --mem=100GB
+#SBATCH --mail-type=ALL
+#SBATCH --output=%x_%j.log 
+module load singularity
+
+singularity run -B /group/testa -B /run/user -B $TMPDIR:/tmp \
+-B /home/marta.sallese -H /home/marta.sallese/ov_cancer_atlas \
+docker://testalab/downstream:covidiamo-3.1.0 \
+"/home/marta.sallese/ov_cancer_atlas/miniconda3/envs/seacells/bin/python /home/marta.sallese/ov_cancer_atlas/out_of_sample/Zheng_fibroblast_metacells.py"
