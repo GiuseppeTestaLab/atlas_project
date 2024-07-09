44705f9ec08463086659103911ad2e965cc52680
Marta Sallese <martarosa.sallese@ieo.it>
2024-07-09 16:15:56 +0200

    chnaged file name

--- script/3_atlas_annotation/1_atlas_rawcounts_cell_labelled.sh
+++ script/3_atlas_annotation/1_atlas_rawcounts_cell_labelled.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=4
 #SBATCH --partition=cpuq
-#SBATCH --job-name=atlas_sub
+#SBATCH --job-name=atlas_def
 #SBATCH --mem=400GB
 #SBATCH --mail-type=ALL
 #SBATCH --output=%x_%j.log 
@@ -12,4 +12,4 @@ module load singularity
 singularity run -B /group/testa -B /run/user -B $TMPDIR:/tmp \
 -B /home/marta.sallese -H /home/marta.sallese/ov_cancer_atlas \
 docker://testalab/downstream:covidiamo-3.1.0 \
-"/bin/python3 /home/marta.sallese/ov_cancer_atlas/atlas_project/script/3_atlas_annotation/atlas_subsets.py"
+"/bin/python3 /home/marta.sallese/ov_cancer_atlas/atlas_project/script/3_atlas_annotation/1_atlas_rawcounts_cell_labelled.py"
