f317577cc1ca555a95cf286f12901407df3ad0c5
Marta Sallese <martarosa.sallese@ieo.it>
2024-07-09 16:19:38 +0200

    changed file names

--- script/2_original_anndata/2_run_atlas_preprocessing.sh
+++ script/2_original_anndata/2_run_atlas_preprocessing.sh
@@ -12,4 +12,4 @@ module load singularity
 singularity run -B /group/testa -B /run/user -B $TMPDIR:/tmp \
 -B /home/marta.sallese -H /home/marta.sallese/ov_cancer_atlas \
 docker://testalab/downstream:covidiamo-3.1.0 \
-"/bin/python3 /home/marta.sallese/ov_cancer_atlas/atlas_project/script/2_original_anndata/atlas_preprocessing.py"
+"/bin/python3 /home/marta.sallese/ov_cancer_atlas/atlas_project/script/2_original_anndata/2_run_atlas_preprocessing.py"
