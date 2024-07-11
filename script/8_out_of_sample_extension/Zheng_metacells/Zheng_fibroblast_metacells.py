e8535f4f1a265aea3d8a567f84e3df60272ff7c8
Marta Sallese <martarosa.sallese@ieo.it>
2024-07-11 12:42:53 +0200

    improved script and added metacell gen

--- script/8_out_of_sample_extension/Zheng_metacells/Zheng_fibroblast_metacells.py
+++ script/8_out_of_sample_extension/Zheng_metacells/Zheng_fibroblast_metacells.py
@@ -6,11 +6,20 @@ import numpy as np
 import pandas as pd
 import scanpy as sc
 import SEACells
+import sys
+sys.path.insert(1, '/home/marta.sallese/ov_cancer_atlas/atlas_project/utils')
+from metacells_derivation import preprocess, assign_metacells, create_mc_matrix, preprocess_mc
+
+## Initialize directories
+initDir = '/group/testa/Project/OvarianAtlas/Zheng2023/Adata/'
+destDir = '/group/testa/Project/OvarianAtlas/Zheng2023/Metacells/'
+sc.settings.set_figure_params(dpi_save=300, frameon=False, format='png')
+sc.settings.figdir = "/home/marta.sallese/ov_cancer_atlas/atlas_project/plots_def/metacells/fibroblasts/"
 
 ## Load data
 #%%
 adata= sc.read("/group/testa/Project/OvarianAtlas/Zheng2023/Adata/zheng_fibroblast_filt_norm_nolog.h5ad")
-
+genes = '/home/marta.sallese/ov_cancer_atlas/Atlas_scripts/HDG_new/Tables/atlas_hdg_dispersion_patients_fibroblasts.csv'
 
 ## Preprocessing
 #%%
@@ -29,55 +38,32 @@ sc.tl.umap(adata)
 
 ## Metacells generation per patient
 #%%
-sc.settings.verbosity = 0
-models = {}
-mc_obs = pd.DataFrame()
-i=0
-for patient in adata.obs.paper_ID.unique():
-    ad_patient=adata[adata.obs.paper_ID==patient].copy()
-    n_SEACells = np.round(ad_patient.shape[0]/75, 0)
-    build_kernel_on = 'X_pca'
-    n_waypoint_eigs = 10 
-    model = SEACells.core.SEACells(ad_patient, 
-                                   build_kernel_on=build_kernel_on, 
-                                   n_SEACells=n_SEACells, 
-                                   n_waypoint_eigs=n_waypoint_eigs, 
-                                   convergence_epsilon = 1e-5,verbose=False)
-    model.construct_kernel_matrix()
-    M = model.kernel_matrix 
-    try:
-        model.initialize_archetypes()
-    except IndexError as e:
-        print('patient {} the number {} has too few cells'.format(patient,i))
-        ad = adata[adata.obs.paper_ID != patient]
-        continue
-    model.fit(min_iter=10, max_iter=200)
-    models[patient]=model
-    labels,weights = model.get_soft_assignments()
-    SEACell_soft_ad = SEACells.core.summarize_by_soft_SEACell(ad_patient, model.A_, celltype_label='paper_ID',summarize_layer='raw', minimum_weight=0.05)
-    if mc_obs.shape[0] == 0:
-        # mc_obs=SEACell_soft_ad
-        mc_obs=ad_patient.obs
-    else:
-        mc_obs=pd.concat([mc_obs,ad_patient.obs])
-        # mc_obs=mc_obs.concatenate(SEACell_soft_ad)
-    i+=1
+adata = assign_metacells(adata)
+adata.write_h5ad(destDir + 'fibroblast_seacells_assignment_hdg_patients.h5ad')
 
-    print('done! patient {} the number {}'.format(patient,i))
-    #if i>2:
-    #    break
-sc.settings.verbosity = 3  
+# Creating metacell matrix
+#%%
+adata = sc.read(destDir + 'fibroblast_seacells_assignment_hdg_patients.h5ad')
+ad = create_mc_matrix(adata)
+#%%
+ad.obs = ad.obs.drop(columns=['n_genes', 'n_genes_by_counts', 
+                    'total_counts', 'total_counts_mt', 'pct_counts_mt', 'EndothelialMSK', 'FibroblastsMSK', 'CancerMSK', 'HematopoieticMSK', 
+                    'cell_labels_ratio', 'max', 'assignment', 'leiden-1.8'])
 
-import pickle
+ad.write(destDir + 'fibroblasts_seacells_hdg_patients.h5ad')
 
-with open('models.pkl','wb') as file:
-    pickle.dump(models,file)
+#%%
+## Compute embeddings and plot metacells
+adata = sc.read(destDir + 'fibroblasts_seacells_hdg_patients.h5ad')
+adata = preprocess_mc(adata, genes)
 
+#%%
+sc.pl.umap(adata, color=["treatment"], frameon=False, save='zheng_seacells_HDG_treatm.png')
+sc.pl.umap(adata, color=["tissue"], frameon=False, save='zheng_seacells_HDG_tissue.png')
+sc.pl.umap(adata, color=["dataset"], frameon=False, save='zheng_seacells_HDG_dataset.png')
+sc.pl.umap(adata, color=["paper_ID"], frameon=False, save='zheng_seacells_HDG_patients.png')
+sc.pl.umap(adata, color=["phase"], frameon=False, save='zheng_seacells_HDG_cellcycle.png')
+sc.pl.umap(adata, color=["anatomical_location"], frameon=False, save='zheng_seacells_HDG_anatomy.png')
 
-with open('models.pkl','rb') as file:
-
-    modelsLoaded=pickle.load(file)
-
-adata.obs['SEACell'] = mc_obs.SEACell
-
-adata.write_h5ad('/group/testa/Project/OvarianAtlas/Zheng2023/Metacells/fibroblast_seacells_assignment_hdg_patients.h5ad')
+#%%
+adata.write(destDir + 'fibroblasts_seacells_hdg_patients_embeddings.h5ad')
