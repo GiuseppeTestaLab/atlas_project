d49b0582e1452ed059dde182827dda17d80aa311
Marta Sallese <martarosa.sallese@ieo.it>
2024-07-12 09:47:22 +0200

    modified scanvi function

--- utils/integration.py
+++ utils/integration.py
@@ -1,7 +1,7 @@
 import scanpy as sc
 import numpy as np
 import pandas as pd
-import scgen
+# import scgen
 
 ## Preprocessing
 def preprocess_scgen(ad, batch, genes):
@@ -25,8 +25,8 @@ def preprocess_scgen_genes(ad, batch, genes):
     return(ad)
 
 def preprocess_scVI(adata, batch, genes):
-    sc.pp.normalize_total(adata, target_sum=1e4)
-    sc.pp.log1p(adata)
+    # sc.pp.normalize_total(adata, target_sum=1e4)
+    # sc.pp.log1p(adata)
     adata.raw = adata
     sc.pp.highly_variable_genes(
         adata,
@@ -35,4 +35,17 @@ def preprocess_scVI(adata, batch, genes):
         batch_key=batch,
         subset=True
         )
+    return(adata)
+
+def preprocess_scVI_genes(adata, batch, genes):
+    # sc.pp.normalize_total(adata, target_sum=1e4)
+    # sc.pp.log1p(adata)
+    adata.raw = adata
+    hdg = pd.read_csv(genes, index_col=0)
+    adata = adata[:, adata.var.highly_variable]
+    adata.var['highly_variable'] = hdg.highly_variable
+    adata.var.highly_variable = adata.var.highly_variable.fillna(False)
+    sc.tl.pca(adata, use_highly_variable=True)
+    sc.pp.neighbors(adata, use_rep='X_pca')
+    sc.tl.umap(adata)
     return(adata)
