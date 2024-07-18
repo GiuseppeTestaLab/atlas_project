be7028b23bc9533637a3e385e750437013aaeba3
Marta Sallese <martarosa.sallese@ieo.it>
2024-07-18 16:18:43 +0200

    integration of immune metacells

---
+++ script/6_integration/metacells/immune/scVI_labels_tissuetreatment_HDG.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --partition=cpuq
+#SBATCH --job-name=scVI_immune
+#SBATCH --mem=200GB
+#SBATCH --mail-type=ALL
+#SBATCH --output=%x_%j.log 
+module load singularity
+
+singularity run -B /group/testa -B /run/user -B $TMPDIR:/tmp \
+-B /home/marta.sallese -H /home/marta.sallese/ov_cancer_atlas \
+docker://testalab/downstream:covidiamo-3.1.0 \
+"/home/marta.sallese/ov_cancer_atlas/miniconda3/envs/scvi/bin/python /home/marta.sallese/ov_cancer_atlas/atlas_project/script/6_integration/metacells/immune/scVI_labels_tissuetreatment_HDG.py"
