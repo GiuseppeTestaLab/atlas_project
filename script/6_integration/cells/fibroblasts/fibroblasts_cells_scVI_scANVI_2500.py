5d8a2dc0ecc31c17f69003d07ce7d6f31202c1ec
Marta Sallese <martarosa.sallese@ieo.it>
2024-07-18 16:19:18 +0200

    integration of fibroblasts cells scVI

--- script/6_integration/cells/fibroblasts/fibroblasts_cells_scVI_scANVI_2500.py
+++ script/6_integration/cells/fibroblasts/fibroblasts_cells_scVI_scANVI_2500.py
@@ -22,6 +22,29 @@ sc.settings.figdir = "/home/marta.sallese/ov_cancer_atlas/atlas_project/plots_de
 adata = sc.read(initDir + 'seacells_assignment_hdg_patients.h5ad')
 adata
 adata.obs['tissue-treatment'] = adata.obs['tissue'].astype('str') + '_' + adata.obs['treatment'].astype('str')
+
+#%%
+# inflammatory CAFs (iCAF)
+sc.tl.score_genes(adata, ['IL6', 'CXCL12', 'VTN', 'CADM3', 'PLIN2', 'SERPINB2', 'KRT19', 'DES', 'CALB2', 'WT1', 'KRT7'], 
+score_name = "iCAF", use_raw=False)
+
+# matrix CAFs (mCAF)
+sc.tl.score_genes(adata, ['ACTA2', 'COL11A1', 'MFAP5', 'SFRP2', 'ISLR', 'COL10A1', 'VIM', 'COL3A', 
+                       'MMP11', 'PTHLH', 'FGF1', 'WNT7B', 'WNT2', 'TGFB3', 'THRC1', 'POSTN', 'VCAN', 'ZEB1'], 
+score_name = "mCAF", use_raw=False)
+
+# vascular cancer-associated fibroblasts CAFs (vCAF)
+sc.tl.score_genes(aadatad, ['COX4I2', 'HIGD1B', 'PTP4A3', 'MCAM', 'PPP1R14A'], 
+score_name = "vCAF", use_raw=False)
+
+# STAR gene expressing CAFs (starCAF)
+sc.tl.score_genes(adata, ['STAR', 'IGFBP5', 'TSPAN8', 'C7', 'ALDH1A1', 'LGR5'], 
+score_name = "starCAF", use_raw=False)
+
+#%%
+adata.obs['cell_types'] = adata.obs[['iCAF', 'mCAF','vCAF', 'starCAF']].idxmax(axis=1)
+adata.obs
+
 batch = "paper_ID"
 genes = 2500
 adata = preprocess_scVI(adata, batch, genes)
@@ -67,20 +90,18 @@ sc.pl.embedding(
     ncols=1,
 )
 
-sc.pl.embedding(adata, basis="X_mde", color=["tissue"], frameon=False, ncols=1)
+sc.pl.embedding(adata, basis="X_mde", color=["cell_types"], frameon=False, ncols=1)
 
 #%%#%%
-adata.write_h5ad(outDir + 'scvi_batch_corr_tissue-treatment_2500.h5ad')
+adata.write_h5ad(outDir + 'scvi_batch_corr_celltypes_2500.h5ad')
 
 ## Integration with scANVI for label key preservation (Treatment)
 
-#%% 
-adata.obs['tissue-treatment'] = adata.obs['tissue'].astype('str') + '_' + adata.obs['treatment'].astype('str')
 #%%
 lvae = scvi.model.SCANVI.from_scvi_model(
     vae,
     adata=adata,
-    labels_key="tissue-treatment",
+    labels_key="cell_types",
     unlabeled_category="Unknown",
 )
 
@@ -93,14 +114,14 @@ adata.obsm["X_scANVI"] = lvae.get_latent_representation(adata)
 adata.obsm["X_mde_scanvi"] = mde(adata.obsm["X_scANVI"])
 
 sc.pl.embedding(
-    adata, basis="X_mde_scanvi", color=["tissue-treatment"], ncols=1, frameon=False
+    adata, basis="X_mde_scanvi", color=["cell_types"], ncols=1, frameon=False
 )
 
 ## visualize data
 sc.tl.pca(adata, use_highly_variable=True)
 sc.pp.neighbors(adata, use_rep='X_pca')
 sc.tl.umap(adata)
-sc.pl.umap(adata, color=['tissue-treatment', 'paper_ID'], frameon=False)
+sc.pl.umap(adata, color=['cell_types', 'paper_ID'], frameon=False)
 
 # %%
-adata.write_h5ad(outDir + 'scanvi_batch_corr_tissue-treatment_2500.h5ad')
+adata.write_h5ad(outDir + 'scanvi_batch_corr_celltypes_2500.h5ad')
