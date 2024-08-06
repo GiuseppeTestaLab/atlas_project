80a7a04b19e513acb2b690b29d09ec40dfbed65c
Marta Sallese <martarosa.sallese@ieo.it>
2024-08-06 15:50:54 +0200

    commented some code

--- script/7_downstream/clustering/immune/3_cluster_assignments/1_immune_clusters_from_metacells.py
+++ script/7_downstream/clustering/immune/3_cluster_assignments/1_immune_clusters_from_metacells.py
@@ -1,7 +1,7 @@
 # Assigning clusters from immune metacells to the original immune cells
 
 ## Objective: Here I will project the clusters from the immune metacells on the original immune cells. I will use the cluster level of resolution identified through the analysis of the Sankey plots.
-
+#%%
 ## Import libraries
 import scanpy as sc
 import pandas as pd
@@ -14,7 +14,7 @@ metacellsDir = '/group/testa/Project/OvarianAtlas/atlas_project/raw_data/metacel
 tissueDir = '/group/testa/Project/OvarianAtlas/atlas_project/raw_data/downstream/clustering/immune/'
 figDir = '/group/testa/Project/OvarianAtlas/atlas_project/raw_data/downstream/clustering/immune/figures/'
 outDir = '/group/testa/Project/OvarianAtlas/atlas_project/raw_data/downstream/clustering/immune/cluster_assignments/'
-
+#%%
 ## Load the data
 cells = sc.read(initDir + 'atlas_immune_filt_norm_nolog.h5ad')
 adata = sc.read(metacellsDir + 'seacells_assignment_hdg_patients.h5ad')
@@ -22,106 +22,103 @@ adata = sc.read(metacellsDir + 'seacells_assignment_hdg_patients.h5ad')
 ## Appending the metacells to the cells they belong to
 cells.obs.index.equals(adata.obs.index)
 
-cells.obs.drop(columns=['ID', 'sample_name', 'patient_id', 'cell_type', 'cell_subtype', 
-                          'sample_ID', 'cell_labels_ratio', 
-                          'assignment', 'leiden-1.8'], inplace = True)
-
+#%%
 cells.obs = pd.concat([cells.obs, adata.obs.SEACell], axis='columns')
 
 cells.obs['SEACell_patient_tissue'] = cells.obs['SEACell'].astype('str') + '_' + cells.obs['paper_ID'].astype('str') + '_' + cells.obs['tissue'].astype('str')
 
 ## immune primary: projecting clusters from metacells on the original cells
+#%%
+# primary = sc.read(tissueDir + 'adata_primary_embeddings.h5ad')
+# primary.obs
 
-primary = sc.read(tissueDir + 'adata_primary_embeddings.h5ad')
-primary.obs
+# primary.obs['leiden-0.xxx'] # 0.xxx is the resolution of the clustering chosen for the primary cells
+# #### The resolution of the clustering is 0.51 or 0.41
 
-primary.obs['leiden-0.xxx'] # 0.xxx is the resolution of the clustering chosen for the primary cells
-#### The resolution of the clustering is 0.51 or 0.41
+# cells.obs['cluster_from_seacells'] = np.nan
+# cluster_name = 'leiden-0.xxx'
+# primary.obs['total_counts_seacell'] = 0
 
-cells.obs['cluster_from_seacells'] = np.nan
-cluster_name = 'leiden-0.xxx'
-primary.obs['total_counts_seacell'] = 0
+# for index, row in primary.obs.iterrows():
+#     cells.obs.loc[(cells.obs['SEACell_patient_tissue'] == index) & (cells.obs['tissue'] == 'Primary'), 'cluster_from_seacells'] = 'Primary_' + row[cluster_name]
+#     primary.obs.loc[index, 'total_counts_seacell'] = cells.obs.loc[(cells.obs['SEACell_patient_tissue'] == index) & (cells.obs['tissue'] == 'Primary'), 'total_counts'].sum()
 
-for index, row in primary.obs.iterrows():
-    cells.obs.loc[(cells.obs['SEACell_patient_tissue'] == index) & (cells.obs['tissue'] == 'Primary'), 'cluster_from_seacells'] = 'Primary_' + row[cluster_name]
-    primary.obs.loc[index, 'total_counts_seacell'] = cells.obs.loc[(cells.obs['SEACell_patient_tissue'] == index) & (cells.obs['tissue'] == 'Primary'), 'total_counts'].sum()
+# cells.obs
 
-cells.obs
+# pd.crosstab(cells.obs.cluster_from_seacells, cells.obs.tissue)
 
-pd.crosstab(cells.obs.cluster_from_seacells, cells.obs.tissue)
+# primary.obs
 
-primary.obs
+# sc.pl.umap(primary, color=['leiden-0.xxx', 'total_counts_seacell'], frameon=False)
 
-sc.pl.umap(primary, color=['leiden-0.xxx', 'total_counts_seacell'], frameon=False)
+# cells.obs.cluster_from_seacells = cells.obs.cluster_from_seacells.astype('str')
 
-cells.obs.cluster_from_seacells = cells.obs.cluster_from_seacells.astype('str')
+# del primary
+# gc.collect()
 
-del primary
-gc.collect()
+# ## immune metastasis: projecting clusters from metacells on the original cells  
 
-## immune metastasis: projecting clusters from metacells on the original cells  
+# metastasis = sc.read(tissueDir + 'adata_metastasis_embeddings.h5ad')
+# metastasis.obs
 
-metastasis = sc.read(tissueDir + 'adata_metastasis_embeddings.h5ad')
-metastasis.obs
+# metastasis.obs['leiden-0.xxx'] # 0.xxx is the resolution of the clustering chosen for the primary cells
+# #### The resolution of the clustering is 0.41 or 0.31
 
-metastasis.obs['leiden-0.xxx'] # 0.xxx is the resolution of the clustering chosen for the primary cells
-#### The resolution of the clustering is 0.41 or 0.31
+# metastasis.obs['total_counts_seacell'] = 0
 
-metastasis.obs['total_counts_seacell'] = 0
+# for index, row in metastasis.obs.iterrows():
+#     cells.obs.loc[(cells.obs['SEACell_patient_tissue'] == index) & (cells.obs['tissue'] == 'Metastasis'), 'cluster_from_seacells'] = 'Metastasis_' + row[cluster_name]
+#     metastasis.obs.loc[index, 'total_counts_seacell'] = cells.obs.loc[(cells.obs['SEACell_patient_tissue'] == index) & (cells.obs['tissue'] == 'Metastasis'), 'total_counts'].sum()
 
-for index, row in metastasis.obs.iterrows():
-    cells.obs.loc[(cells.obs['SEACell_patient_tissue'] == index) & (cells.obs['tissue'] == 'Metastasis'), 'cluster_from_seacells'] = 'Metastasis_' + row[cluster_name]
-    metastasis.obs.loc[index, 'total_counts_seacell'] = cells.obs.loc[(cells.obs['SEACell_patient_tissue'] == index) & (cells.obs['tissue'] == 'Metastasis'), 'total_counts'].sum()
+# cells.obs
 
-cells.obs
+# pd.crosstab(cells.obs.cluster_from_seacells, cells.obs.tissue)
 
-pd.crosstab(cells.obs.cluster_from_seacells, cells.obs.tissue)
+# metastasis.obs
 
-metastasis.obs
+# sc.pl.umap(metastasis, color=['leiden-0.xxx', 'total_counts_seacell'], frameon=False)
 
-sc.pl.umap(metastasis, color=['leiden-0.xxx', 'total_counts_seacell'], frameon=False)
+# cells.obs.cluster_from_seacells = cells.obs.cluster_from_seacells.astype('str')
 
-cells.obs.cluster_from_seacells = cells.obs.cluster_from_seacells.astype('str')
+# del metastasis
+# gc.collect()
 
-del metastasis
-gc.collect()
+# ## immune ascites: projecting clusters from metacells on the original cells
 
-## immune ascites: projecting clusters from metacells on the original cells
+# ascites = sc.read(tissueDir + 'adata_ascites_embeddings.h5ad')
+# ascites.obs
 
-ascites = sc.read(tissueDir + 'adata_ascites_embeddings.h5ad')
-ascites.obs
+# ascites.obs['leiden-0.xxx'] # 0.xxx is the resolution of the clustering chosen for the primary cells
 
-ascites.obs['leiden-0.xxx'] # 0.xxx is the resolution of the clustering chosen for the primary cells
+# ascites.obs['total_counts_seacell'] = 0
 
-ascites.obs['total_counts_seacell'] = 0
+# for index, row in ascites.obs.iterrows():
+#     cells.obs.loc[(cells.obs['SEACell_patient_tissue'] == index) & (cells.obs['tissue'] == 'Ascites'), 'cluster_from_seacells'] = 'Ascites_' + row[cluster_name]
+#     ascites.obs.loc[index, 'total_counts_seacell'] = cells.obs.loc[(cells.obs['SEACell_patient_tissue'] == index) & (cells.obs['tissue'] == 'Ascites'), 'total_counts'].sum()
 
-for index, row in ascites.obs.iterrows():
-    cells.obs.loc[(cells.obs['SEACell_patient_tissue'] == index) & (cells.obs['tissue'] == 'Ascites'), 'cluster_from_seacells'] = 'Ascites_' + row[cluster_name]
-    ascites.obs.loc[index, 'total_counts_seacell'] = cells.obs.loc[(cells.obs['SEACell_patient_tissue'] == index) & (cells.obs['tissue'] == 'Ascites'), 'total_counts'].sum()
+# cells.obs
 
-cells.obs
+# pd.crosstab(cells.obs.cluster_from_seacells, cells.obs.tissue)
 
-pd.crosstab(cells.obs.cluster_from_seacells, cells.obs.tissue)
+# ascites.obs
 
-ascites.obs
+# sc.pl.umap(ascites, color=['leiden-0.xxx', 'total_counts_seacell'], frameon=False)
 
-sc.pl.umap(ascites, color=['leiden-0.xxx', 'total_counts_seacell'], frameon=False)
+# cells.obs.cluster_from_seacells = cells.obs.cluster_from_seacells.astype('str')
 
-cells.obs.cluster_from_seacells = cells.obs.cluster_from_seacells.astype('str')
+# del ascites
+# gc.collect()
 
-del ascites
-gc.collect()
+# ## Plotting the clusters from metacells on the original cells
 
-## Plotting the clusters from metacells on the original cells
+# sc.pl.umap(cells, color=['tissue', 'cluster_from_seacells'], frameon=False, save='_immune_clusters_from_metacells.png')
 
-sc.pl.umap(cells, color=['tissue', 'cluster_from_seacells'], frameon=False, save='_immune_clusters_from_metacells.png')
+# ## Checking for NA values
 
-## Checking for NA values
+# cells.obs.SEACell.isna().sum()
+# cells.obs.SEACell_patient_tissue.isna().sum()
 
-cells.obs.SEACell.isna().sum()
-cells.obs.SEACell_patient_tissue.isna().sum()
+# ## Saving the data
 
-## Saving the data
-
-cells.write_h5ad(outDir + 'atlas_immune_clusters_from_seacells.h5ad')
+# cells.write_h5ad(outDir + 'atlas_immune_clusters_from_seacells.h5ad')
 
