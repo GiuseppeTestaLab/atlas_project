d2defa70256bffb3de0a378acbe96f7342a170f9
Marta Sallese <martarosa.sallese@ieo.it>
2024-08-12 17:28:04 +0200

    edit files

--- script/3_atlas_annotation/clustering_metrics_plots.py
+++ script/3_atlas_annotation/clustering_metrics_plots.py
@@ -4,6 +4,7 @@ from sklearn import metrics
 import scanpy as sc
 import pandas as pd
 import seaborn as sns
+import os
 
 #%%
 initDir = '/group/testa/Project/OvarianAtlas/atlas_project/raw_data/atlas_annotated/'
@@ -17,13 +18,23 @@ palette = {
 }
 
 #%%
-data = ['clustering_metrics_cancer.csv', 'clustering_metrics_endothelial.csv', 'clustering_metrics_fibroblasts.csv', 'clustering_metrics_immune.csv']
+data = [
+        # 'clustering_metrics_cancer.csv', 
+        # 'clustering_metrics_endothelial.csv', 
+        # 'clustering_metrics_fibroblasts.csv', 
+        'clustering_metrics_immune.csv'
+        ]
 
+#%%
 for i in data:
     df = pd.read_csv(initDir + i)
     df.columns = ['Leiden', 'Adjusted Rand Index', 'Adjusted Mutual Information', 'Fowlkes-Mallows Index']
     df = df.set_index('Leiden')
     ax = sns.lineplot(data=df, palette=palette)
     ax.set(xlabel='Leiden Resolution', ylabel='Score')
-    ax.set_title(f'Clustering Metrics for {i.split("_")[2]} Cells')
-    plt.savefig(figDir + 'clustering_metrics_' + i.split('_')[2] + '.png')
+    # ax.set_title(f'Clustering Metrics for {i.split("_")[2].capitalize()} Cells')
+    output_filename = os.path.join(figDir, i.replace('.csv', '_plot.pdf'))
+    ax.figure.savefig(output_filename)
+    ax.figure.clear()
+
+# %%
