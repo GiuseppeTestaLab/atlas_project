118c1705d30f1ddac6cd47d0f0a2f0d457f3e07d
emanuele <emanuele.villa@fht.org>
2024-09-03 10:31:59 +0200

    refactor initdir

--- script/1_original_counts/2_aggregate_datasets.py
+++ script/1_original_counts/2_aggregate_datasets.py
@@ -11,33 +11,23 @@ import logging
 init_dir = '/group/testa/Project/OvarianAtlas/atlas_project/raw_data/original_counts/'
 #%%
 ## Aggregating datasets
-adata = sc.read(init_dir + 'Geistlinger2020/Adata/geistlinger2020_rawcounts.h5ad')
-# #%%
-# datasets=["loret2022",
-#             "olbrecht2021",
-#             "qian2020",
-#             "regner2021",
-#             "ren2022",
-#             "vasquez2022",
-#             "xu2022",
-#             "zhang2022"]
 
-# files_path = os.listdir(init_dir)
-# listpath = [os.path.join(init_dir, file, 'Adata/') for file in files_path]
+## load the first
+adata = sc.read(init_dir + 'Geistlinger2020/Adata/geistlinger2020_rawcounts.h5ad')
 
 #%%
-data = ['/group/testa/Project/OvarianAtlas/atlas_project/raw_data/original_counts/Loret2022/Adata/loret2022_rawcounts.h5ad',
-        '/group/testa/Project/OvarianAtlas/atlas_project/raw_data/original_counts/Olbrecht2021/Adata/olbrecht2021_rawcounts.h5ad',
-        '/group/testa/Project/OvarianAtlas/atlas_project/raw_data/original_counts/Xu2022/Adata/xu2022_rawcounts.h5ad',
-        '/group/testa/Project/OvarianAtlas/atlas_project/raw_data/original_counts/Zhang2022/Adata/zhang2022_rawcounts.h5ad',
-        '/group/testa/Project/OvarianAtlas/atlas_project/raw_data/original_counts/Regner2021/Adata/regner2021_rawcounts.h5ad',
-        '/group/testa/Project/OvarianAtlas/atlas_project/raw_data/original_counts/Ren2022/Adata/ren2022_rawcounts.h5ad',
-        '/group/testa/Project/OvarianAtlas/atlas_project/raw_data/original_counts/Qian2020/Adata/qian2020_rawcounts.h5ad',
-        '/group/testa/Project/OvarianAtlas/atlas_project/raw_data/original_counts/Vasquez2022/Adata/vasquez2022_rawcounts.h5ad']
+data = [init_dir + 'Loret2022/Adata/loret2022_rawcounts.h5ad',
+        init_dir + 'Olbrecht2021/Adata/olbrecht2021_rawcounts.h5ad',
+        init_dir + 'Xu2022/Adata/xu2022_rawcounts.h5ad',
+        init_dir + 'Zhang2022/Adata/zhang2022_rawcounts.h5ad',
+        init_dir + 'Regner2021/Adata/regner2021_rawcounts.h5ad',
+        init_dir + 'Ren2022/Adata/ren2022_rawcounts.h5ad',
+        init_dir + 'Qian2020/Adata/qian2020_rawcounts.h5ad',
+        init_dir + 'Vasquez2022/Adata/vasquez2022_rawcounts.h5ad']
 
 for i in data:
     adata = adata.concatenate(sc.read(i), index_unique=None)
 
 adata.obs = adata.obs.drop(columns=['batch'])
 
-adata.write_h5ad('/group/testa/Project/OvarianAtlas/atlas_project/raw_data/original_counts/atlas_rawcounts.h5ad')
+adata.write_h5ad(init_dir + 'atlas_rawcounts.h5ad')
