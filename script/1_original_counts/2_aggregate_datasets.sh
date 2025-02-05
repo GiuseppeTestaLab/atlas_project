6a6b04dc62e0b002c2ed4ce06e6c34531fffc404
Vittorio Aiello <vittorio.aiello@fht.org>
2025-02-05 17:17:59 +0100

    update aggregate

--- script/1_original_counts/2_aggregate_datasets.sh
+++ script/1_original_counts/2_aggregate_datasets.sh
@@ -1,33 +1,28 @@
 #!/bin/bash
-#SBATCH --time=24:00:00
+#SBATCH --time=00:10:00
 #SBATCH --nodes=1
-#SBATCH --ntasks=4
+#SBATCH --ntasks=2
 #SBATCH --partition=cpuq
 #SBATCH --job-name=aggregate
-#SBATCH --mem=400GB
+#SBATCH --mem=86GB
 #SBATCH --mail-type=ALL
-#SBATCH --output=%x_%j.log 
-module load singularity
-
-# Check if dataset name is provided
-if [ -z "$1" ]; then
-  echo "No dataset name provided"
-  exit 1
-fi
-
-dataset=$1
+#SBATCH --output=logs/%x_%j.log
 
 # Load configuration file
-source config.ini
+source ../../utils/bash_ini_parser/read_ini.sh
+read_ini ../../utils/config.ini
 
 # Set environment variables from the configuration file
-initialPath=${DEFAULT_initialPath}
-scriptsPath=${initialPath}/${scriptsPath}
-bindPaths=$(echo ${SINGULARITY_bindPaths} | tr ',' ' ')
-homePath=${SINGULARITY_homePath}
-image=${SINGULARITY_image}
+scriptsPath=${INI__DEFAULT__scriptsPath}
+bindPaths=${INI__SINGULARITY__bindPaths}
+bindPaths=$(eval echo $bindPaths)
+homePath=${INI__SINGULARITY__homePath}
+image=${INI__SINGULARITY__image}
+
+echo script=${scriptsPath} bind=${bindPaths} home=${homePath} image=${image}
 
 module load singularity
 
-singularity run -B $bindPaths -H $homePath $image \
-"/bin/python3 ${scriptsPath}1_original_counts/2_aggregate_datasets.py"
+singularity exec -B $bindPaths -H $homePath $image \
+                 /bin/bash -c "source ~/.bashrc && mamba activate ovarian && python3 ${scriptsPath}1_original_counts/2_aggregate_datasets.py"
+
