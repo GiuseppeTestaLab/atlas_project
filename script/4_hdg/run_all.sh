377a50cbf07d60cfd9868e07eaa14ed7100377a0
Vittorio Aiello <vittorio.aiello@fht.org>
2025-02-07 11:59:12 +0100

    fix: path

--- script/4_hdg/run_all.sh
+++ script/4_hdg/run_all.sh
@@ -5,5 +5,5 @@ sbatch immune/1_dispersion_table_all_datasets_immune.sh
 sbatch immune/2_hvg_union_patients_dispersion_immune.sh
 sbatch endothelial/1_dispersion_table_all_datasets_endothelial.sh
 sbatch endothelial/2_hvg_union_patients_dispersion_endothelial.sh
-sbatch fibroblast/1_dispersion_table_all_datasets_fibroblast.sh 
-sbatch fibroblast/2_hvg_union_patients_dispersion_fibroblast.sh
+sbatch fibroblasts/1_dispersion_table_all_datasets_fibroblast.sh 
+sbatch fibroblasts/2_hvg_union_patients_dispersion_fibroblast.sh
