4a7c92ae6dabe61fc02a24e9a6ebea1c21acade4
Marta Sallese <martarosa.sallese@ieo.it>
2025-02-09 20:50:08 +0100

    fix for cell subtypes

--- script/8_out_of_sample_extension/label_transfer_immune.py
+++ script/8_out_of_sample_extension/label_transfer_immune.py
@@ -29,32 +29,31 @@ adata
 adata.obs
 
 #%%
-primary = sc.read_h5ad(mcDir + 'adata_primary_embeddings_cellstates.h5ad')
-ascites = sc.read_h5ad(mcDir + 'adata_ascites_embeddings_cellstates.h5ad')
-metastasis = sc.read_h5ad(mcDir + 'adata_metastasis_embeddings_cellstates.h5ad')
+primary = sc.read_h5ad(mcDir + 'adata_primary_embeddings.h5ad')
+ascites = sc.read_h5ad(mcDir + 'adata_ascites_embeddings.h5ad')
+metastasis = sc.read_h5ad(mcDir + 'adata_metastasis_embeddings.h5ad')
 
 #%%
-# Extract cell states
-primary_states = primary.obs[['cell_states']].rename(columns={'cell_states': 'primary_state'})
-ascites_states = ascites.obs[['cell_states']].rename(columns={'cell_states': 'ascites_state'})
-metastasis_states = metastasis.obs[['cell_states']].rename(columns={'cell_states': 'metastasis_state'})
+# Extract cell subtypes
+primary_states = primary.obs[['cell_subtypes']].rename(columns={'cell_subtypes': 'primary_state'})
+ascites_states = ascites.obs[['cell_subtypes']].rename(columns={'cell_subtypes': 'ascites_state'})
+metastasis_states = metastasis.obs[['cell_subtypes']].rename(columns={'cell_subtypes': 'metastasis_state'})
 
 #%%
 # Merge with adata.obs
 adata.obs = adata.obs.join(primary_states, how='left').join(ascites_states, how='left').join(metastasis_states, how='left')
 
 #%%
-# Create a final 'cell_states' column combining all sources, prioritizing primary > ascites > metastasis
-adata.obs['cell_states'] = adata.obs['primary_state'].combine_first(adata.obs['ascites_state']).combine_first(adata.obs['metastasis_state'])
-
+# Create a final 'cell_subtypes' column combining all sources, prioritizing primary > ascites > metastasis
+adata.obs['cell_subtypes'] = adata.obs['primary_state'].combine_first(adata.obs['ascites_state']).combine_first(adata.obs['metastasis_state'])
 
 #%%
-sc.pl.umap(adata, color='cell_states')
+sc.pl.umap(adata, color='cell_subtypes')
 
 #%%
 sc.pp.neighbors(adata, use_rep="latent_corrected")
 sc.tl.umap(adata)
-sc.pl.umap(adata, color='cell_states')
+sc.pl.umap(adata, color='cell_subtypes')
 
 #%%
 atlas_adata = adata[adata.obs["reference_map"] == "reference"]
@@ -63,7 +62,7 @@ new_dataset_adata = adata[adata.obs["reference_map"] != "reference"]
 #%%
 # Extract latent space embeddings
 X_train = atlas_adata.obsm["latent_corrected"]
-y_train = atlas_adata.obs["cell_states"].to_numpy()
+y_train = atlas_adata.obs['cell_subtypes'].to_numpy()
 
 X_test = new_dataset_adata.obsm["latent_corrected"]
 
@@ -74,13 +73,13 @@ knn.fit(X_train, y_train)
 
 #%%
 # Predict labels for new dataset
-new_dataset_adata.obs["predicted_cell_states"] = knn.predict(X_test)
+new_dataset_adata.obs['predicted_cell_subtypes'] = knn.predict(X_test)
 
 #%%
-sc.pl.umap(new_dataset_adata, color=["predicted_cell_states"], frameon=False, save="_oose_predicted_cell_states.png")
+sc.pl.umap(new_dataset_adata, color=["predicted_cell_subtypes"], frameon=False, save="_oose_predicted_cell_states.png")
 
 #%%
 # Save adata
-adata.write_h5ad(ooseDir + 'integrated_query_zheng_seacells_scarches_tissuetreat_predicted_cellstates.h5ad')
+adata.write_h5ad(ooseDir + 'integrated_query_zheng_seacells_scarches_tissuetreat_predicted_cellsubtypes.h5ad')
 
 #%%
