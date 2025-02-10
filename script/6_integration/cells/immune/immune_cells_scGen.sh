5126a16b068320cd154f3e1cfd042c05cbb945e8
Vittorio Aiello <vittorio.aiello@fht.org>
2025-02-10 15:19:59 +0100

    fix: scgen scripts

--- script/6_integration/cells/immune/immune_cells_scGen.sh
+++ script/6_integration/cells/immune/immune_cells_scGen.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --time=120:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks=8
 #SBATCH --ntasks-per-node=8
 #SBATCH --gres=gpu:1
+#SBATCH --partition=gpuq
 #SBATCH --job-name=cells_integr
 #SBATCH --mem=300GB
 #SBATCH --mail-type=ALL
