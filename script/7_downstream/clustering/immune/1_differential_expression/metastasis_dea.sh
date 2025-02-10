ff7809732082aa5098e2458a230688b58b2c5440
Vittorio Aiello <vittorio.aiello@fht.org>
2025-02-10 01:05:23 +0100

    update scripts

--- script/7_downstream/clustering/immune/1_differential_expression/metastasis_dea.sh
+++ script/7_downstream/clustering/immune/1_differential_expression/metastasis_dea.sh
@@ -9,7 +9,5 @@
 #SBATCH --output=logs/%x_%j.log
 module load singularity
 
-singularity run -B /group/testa -B /run/user -B $TMPDIR:/tmp \
--B /home/marta.sallese -H /home/marta.sallese/ov_cancer_atlas \
-docker://testalab/downstream:covidiamo-3.1.0 \
-"/home/marta.sallese/miniconda3/envs/gprofiler/bin/python /home/marta.sallese/ov_cancer_atlas/atlas_project/script/7_downstream/clustering/immune/1_differential_expression/Metastasis_dea.py"
+singularity exec -B $bindPaths -H $homePath $image \
+                 /bin/bash -c "eval \"\$(conda shell.bash hook)\" && conda activate downstream && python3 ${scriptsPath}7_downstream/clustering/immune/1_differential_expression/Metastasis_dea.py"
