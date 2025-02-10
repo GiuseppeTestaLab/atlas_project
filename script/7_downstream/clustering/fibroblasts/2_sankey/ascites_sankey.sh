53ed6dda22e244e096d90bd995d2a59be06e890b
Vittorio Aiello <vittorio.aiello@fht.org>
2025-02-10 01:08:35 +0100

    added env variables

--- script/7_downstream/clustering/fibroblasts/2_sankey/ascites_sankey.sh
+++ script/7_downstream/clustering/fibroblasts/2_sankey/ascites_sankey.sh
@@ -9,5 +9,16 @@
 #SBATCH --output=logs/%x_%j.log
 module load singularity
 
+# Load configuration file
+source ../../utils/bash_ini_parser/read_ini.sh
+read_ini ../../utils/config.ini
+
+# Set environment variables from the configuration file
+scriptsPath=${INI__DEFAULT__scriptsPath}
+bindPaths=${INI__SINGULARITY__bindPaths}
+bindPaths=$(eval echo $bindPaths)
+homePath=${INI__SINGULARITY__homePath}
+image=${INI__SINGULARITY__image}
+
 singularity exec -B $bindPaths -H $homePath $image \
                  /bin/bash -c "eval \"\$(conda shell.bash hook)\" && conda activate downstream && python3 ${scriptsPath}7_downstream/clustering/fibroblasts/2_sankey/ascites_sankey.py"
