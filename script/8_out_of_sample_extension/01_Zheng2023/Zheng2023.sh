fccb17d993a268292918ba66f00131bf8d46e736
Vittorio Aiello <vittorio.aiello@fht.org>
2025-02-16 23:58:57 +0100

    added script to run zheng

---
+++ script/8_out_of_sample_extension/01_Zheng2023/Zheng2023.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=2
+#SBATCH --partition=cpuq
+#SBATCH --job-name=zheng2023
+#SBATCH --mem=128GB
+#SBATCH --mail-type=ALL
+#SBATCH --output=logs/%x_%j.log
+
+
+# Load configuration file
+# Load configuration file
+source ../../utils/bash_ini_parser/read_ini.sh
+read_ini ../../utils/config.ini
+
+# Set environment variables from the configuration file
+scriptsPath=${INI__DEFAULT__scriptsPath}
+datasetPath=${INI__DEFAULT__scriptsPath}1_original_counts/${dataset}
+bindPaths=${INI__SINGULARITY__bindPaths}
+bindPaths=$(eval echo $bindPaths)
+homePath=${INI__SINGULARITY__homePath}
+image=${INI__SINGULARITY__image}
+
+echo script=${scriptsPath} dataset=${datasetPath} bind=${bindPaths} home=${homePath} image=${image}
+
+module load singularity
+
+singularity exec -B $bindPaths -H $homePath $image \
+                 /bin/bash -c "eval \"\$(conda shell.bash hook)\" && conda activate ovarian && \
+                 python3 01_Zheng2023/1_Zheng.py && \
+                 python3 01_Zheng2023/2_Zheng_cell_labelling.py && \
+                 python3 01_Zheng2023/3_Zheng_cell_subsets.py"
