566b854778903504755c36406dfb76451b59698e
Vittorio Aiello <vittorio.aiello@fht.org>
2025-02-17 11:45:15 +0100

    added run_all

---
+++ script/8_out_of_sample_extension/02_Zheng_hdg/run_all.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sbatch 02_Zheng_hdg/1_hdg_zheng_cancer.sh
+sbatch 02_Zheng_hdg/1_hdg_zheng_endothelial.sh
+sbatch 02_Zheng_hdg/1_hdg_zheng_fibroblast.sh
+sbatch 02_Zheng_hdg/1_hdg_zheng_immune.sh
