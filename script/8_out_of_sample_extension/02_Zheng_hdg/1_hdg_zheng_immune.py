35fc4aa50622b21fa3544a0e9d35c1239ae79030
Vittorio Aiello <vittorio.aiello@fht.org>
2025-02-17 11:40:15 +0100

    added hdgs for all celltypes

---
+++ script/8_out_of_sample_extension/02_Zheng_hdg/1_hdg_zheng_immune.py
@@ -0,0 +1,105 @@
+
+
+#%%
+import scanpy as sc
+import pandas as pd
+import numpy as np
+import glob
+import sys
+import configparser
+
+# Read configuration file
+config = configparser.ConfigParser()
+config.read("../../utils/config.ini")
+
+utilsPath = config.get("DEFAULT", "utilsPath")
+rawPath = config.get("DEFAULT", "rawPath")
+scriptsPath = config.get("DEFAULT", "scriptsPath")
+
+sys.path.insert(1, utilsPath)
+from cell_labeller import assign_scores, actual_labeller, create_immune_adata
+
+## Initialize directories
+initDir = rawPath + 'original_counts/Zheng2023/Adata/'
+
+#%%
+## Zheng
+adata = sc.read(initDir + 'zheng2023_embeddings_cell_labelled.h5ad')
+
+#%%
+### I will take as var names the var names of this dataset
+common_var_names = adata.var_names.tolist()
+#%%
+## Cell labelling strategy
+# adata = assign_scores(adata)
+# adata = actual_labeller(adata)
+adata_immune = create_immune_adata(adata)
+
+adata_immune.write(initDir + 'zheng2023_adata_immune.h5ad')
+
+#%%
+dataDir = rawPath + 'original_counts/'
+dataName = ['zheng2023']
+
+# common_var_names = pd.read_csv(scriptsPath + '4_hdg/Tables/common_varnames_datasets.csv', index_col=0)
+
+dispersion_table = pd.DataFrame(index = common_var_names)
+hvg_table = pd.DataFrame(index = common_var_names)
+
+for j in dataName:
+    path = (dataDir + j.capitalize() + '/Adata' + "/{}_adata_immune.h5ad").format(j)
+    print(path)
+    adata_immune = sc.read(path)
+    dispersion_gene_xpatient = {}
+    highly_variable_genes_per_patient = {}
+    for patient_code in adata_immune.obs["paper_ID"].unique():
+        patient_anndata = adata_immune[adata_immune.obs["paper_ID"] == patient_code].copy()
+        sc.pp.highly_variable_genes(patient_anndata, min_mean=0.0125, max_mean=3, min_disp=0.5)
+        highly_variable_genes_per_patient[patient_code] = patient_anndata.var.highly_variable
+        dispersion_gene_xpatient[patient_code] = patient_anndata.var.dispersions_norm
+    for i in dispersion_gene_xpatient:
+        print(i)
+        print(dispersion_gene_xpatient[i])
+        dispersion_table[i] = dispersion_gene_xpatient[i]
+        hvg_table[i] = highly_variable_genes_per_patient[i]
+
+dispersion_table.to_csv(scriptsPath + '4_hdg/Tables/dispersion_table_immune_zheng.csv')
+hvg_table.to_csv(scriptsPath + '4_hdg/Tables/hvg_table_immune_zheng.csv')
+
+# 2_hvg_union_patients_dispersion_immune zheng
+## Computing HDG by patient based on dispersion values
+
+dir = scriptsPath + '4_hdg/Tables/'
+#%%
+dispersion_table = pd.read_csv(scriptsPath + '4_hdg/Tables/dispersion_table_immune_zheng.csv', index_col=0)
+
+#%%
+dispersion_table
+
+#%%
+max_values_per_row = dispersion_table.max(axis=1)
+
+print(max_values_per_row)
+
+#%%
+#filtered_df = dispersion_table[dispersion_table > 2.3].dropna(how='all')  # 5028 genes
+filtered_df = dispersion_table[dispersion_table > 1.65].dropna(how='all')  # 5302 genes
+
+#%%
+list_of_genes = filtered_df.index.tolist()
+print(len(list_of_genes))
+
+#%%
+hdg_table = pd.DataFrame(index=dispersion_table.index)
+
+#%%
+hdg_table['highly_variable'] = hdg_table.index.isin(list_of_genes)
+
+#%%
+hdg_table.to_csv(dir + 'atlas_hdg_common_dispersion_patients_immune_zheng.csv')
+
+#%%
+hdg_true = pd.DataFrame(index=list_of_genes)
+hdg_true['highly_variable'] = "True"
+hdg_true.to_csv(dir + 'atlas_hdg_dispersion_patients_immune_zheng.csv')
+
---
+++ script/8_out_of_sample_extension/02_Zheng_hdg/1_hdg_zheng_immune.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=2
+#SBATCH --partition=cpuq
+#SBATCH --job-name=hdg_zheng_can
+#SBATCH --mem=128GB
+#SBATCH --mail-type=ALL
+#SBATCH --output=logs/%x_%j.log
+
+
+# Load configuration file
+# Load configuration file
+source ../../utils/bash_ini_parser/read_ini.sh
+read_ini ../../utils/config.ini
+
+# Set environment variables from the configuration file
+scriptsPath=${INI__DEFAULT__scriptsPath}
+datasetPath=${INI__DEFAULT__scriptsPath}1_original_counts/${dataset}
+bindPaths=${INI__SINGULARITY__bindPaths}
+bindPaths=$(eval echo $bindPaths)
+homePath=${INI__SINGULARITY__homePath}
+image=${INI__SINGULARITY__image}
+
+echo script=${scriptsPath} dataset=${datasetPath} bind=${bindPaths} home=${homePath} image=${image}
+
+module load singularity
+
+singularity exec -B $bindPaths -H $homePath $image \
+                 /bin/bash -c "eval \"\$(conda shell.bash hook)\" && conda activate ovarian && \
+                 python3 02_Zheng_hdg/1_hdg_zheng_immune.py"
