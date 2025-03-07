b2c5d503dc04355bc5bb177a4c6613b7427a5655
Marta Sallese <martarosa.sallese@ieo.it>
2025-03-07 12:36:56 +0100

    endothelial test marta

---
+++ script/5_metacells/endothelial/atlas_endothelial_xpatient_hdg_bydispersion_TEST.py
@@ -0,0 +1,61 @@
+# Atlas endothelial metacells generation
+
+## Import libraries
+#%%
+import numpy as np
+import pandas as pd
+import scanpy as sc
+import SEACells
+
+#%%
+import sys
+sys.path.insert(1, '/home/marta.sallese/ov_cancer_atlas/atlas_project/utils')
+from metacells_derivation_TEST import preprocess, assign_metacells, create_mc_matrix, preprocess_mc
+
+#%%
+initDir = '/group/testa/Project/OvarianAtlas/atlas_project/raw_data/atlas_annotated_backup/atlas_annotated/'
+destDir = '/group/testa/Project/OvarianAtlas/atlas_project/raw_data/metacells_backup/metacells/endothelial/'
+
+## Load data
+#%%
+adata = sc.read(initDir + "atlas_endothelial_filt_norm_nolog.h5ad")
+genes = '/home/marta.sallese/ov_cancer_atlas/atlas_project/script/4_hdg/Tables/atlas_hdg_dispersion_patients_endothelial.csv'
+
+## Preprocessing
+#%%
+adata = preprocess(adata, genes)
+adata.write_h5ad(initDir + 'atlas_endothelial_embeddings_TEST270225.h5ad')
+
+## Metacells generation per patient
+#%%
+adata = assign_metacells(adata)
+adata.write_h5ad(destDir + 'seacells_assignment_hdg_patients_TEST270225.h5ad')
+
+# Creating metacell matrix
+# #%%
+# adata = sc.read(destDir + 'seacells_assignment_hdg_patients.h5ad')
+
+# #%%
+# ad = create_mc_matrix(adata)
+# ad.write(destDir + 'seacells_hdg_patients.h5ad')
+
+# ## Compute embeddings and plot metacells
+
+# #%%
+# sc.settings.set_figure_params(dpi_save=300, frameon=False, format='png')
+# sc.settings.figdir = figPath + "metacells/endothelial/"
+
+# #%%
+# adata = sc.read(destDir + 'seacells_hdg_patients.h5ad')
+# adata = preprocess_mc(adata, genes)
+
+# #%%
+# sc.pl.umap(adata, color=["treatment"], frameon=False, save='endothelial_seacells_HDG_treatm.png')
+# sc.pl.umap(adata, color=["tissue"], frameon=False, save='endothelial_seacells_HDG_tissue.png')
+# sc.pl.umap(adata, color=["dataset"], frameon=False, save='endothelial_seacells_HDG_dataset.png')
+# sc.pl.umap(adata, color=["paper_ID"], frameon=False, save='endothelial_seacells_HDG_patients.png')
+# sc.pl.umap(adata, color=["phase"], frameon=False, save='endothelial_seacells_HDG_cellcycle.png')
+# sc.pl.umap(adata, color=["anatomical_location"], frameon=False, save='endothelial_seacells_HDG_anatomy.png')
+
+# #%%
+# adata.write(destDir + 'seacells_hdg_patients_embeddings.h5ad')
---
+++ script/5_metacells/endothelial/atlas_endothelial_xpatient_hdg_bydispersion_TEST.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --time=144:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=8
+#SBATCH --partition=cpuq
+#SBATCH --job-name=endothelial
+#SBATCH --mem=300GB
+#SBATCH --mail-type=ALL
+#SBATCH --output=%x_%j.log
+#SBATCH --error=%x_%j.log
+
+module load singularity
+
+singularity run -B /group/testa -B /run/user -B $TMPDIR:/tmp \
+-B /home/marta.sallese -H /home/marta.sallese/ov_cancer_atlas \
+docker://testalab/downstream:covidiamo-3.1.0 \
+"/home/marta.sallese/ov_cancer_atlas/miniconda3/envs/seacells/bin/python /home/marta.sallese/ov_cancer_atlas/atlas_project/script/5_metacells/endothelial/atlas_endothelial_xpatient_hdg_bydispersion_TEST.py"
